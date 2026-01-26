b78d99985d6940868a7b7db35a1d2ad98ca09c4f
CryoNova <cmnova1xo@gmail.com>
2026-01-26 03:14:33 -0600

    Update Home.py

--- streamlit_app.py/Home.py/Home.py
+++ streamlit_app.py/Home.py/Home.py
@@ -28,7 +28,7 @@ with col1:
 
 with col2:
     st.subheader("🏁 OfficialReferee Factor")
-    ref_style = st.radio("Official's Whistle Tendency", ["Tight(Foul Heavy)", "Average", "Loose" ("Plays On")])
+    ref_style = st.radio("Official's Whistle Tendency", ["Tight(Foul Heavy)", "Average", "Loose(Plays On)"])
     
     if ref_style == "Tight" ("Foul Heavy"):
         st.info("📈 Advantage OVER. Tight whistles lead to more Free ThrowsPenalties.")
@@ -42,6 +42,7 @@ if st.button("Calculate Global Confidence"):
     confidence = 85 if spread >10 and ref_style == "Tight" ("Foul Heavy") else 55
 
     st.metric("Global Hit Probability", f"{confidence}%")
+
 
 
 
